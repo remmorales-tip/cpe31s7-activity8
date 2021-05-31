2b1a25d444985af17c223369d425c45f37075182
remmorales-tip <qremmorales@tip.edu.ph>
2021-05-31 12:52:14 +0000

    Activity 8 contents

--- user.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-sudo cp /vagrant/keys/* /home/vagrant/.ssh/
-sudo chown -R vagrant. /home/vagrant/.ssh/
-sudo chmod 600 /home/vagrant/.ssh/id_rsa
-sudo chmod 644 /home/vagrant/.ssh/id_rsa.pub
-sudo cat /home/vagrant/.ssh/id_rsa.pub | tee -a /home/vagrant/.ssh/authorized_keys
